dd8575c9ffff8150aac9e6c4de2fe94b6357517b
Kwanchaiz <Kwanchaiz2543@gmail.com>
2022-02-28 13:52:51 +0700

    Kwanchai
    input script

--- ShowDasbord.php
+++ ShowDasbord.php
@@ -18,6 +18,11 @@
   <!-- Latest compiled JavaScript -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
+
+  <!-- Sweet Alert -->
+  <script src="sweetalert2.min.js"></script>
+  <link rel="stylesheet" href="sweetalert2.min.css">
+
   <meta name="Description" content="HomePage">
 
   <title>Votezone Nakhonratchasima organization</title>
--- home.php
+++ home.php
@@ -16,7 +16,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Latest compiled JavaScript -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <scrip src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
   <meta name="Description" content="HomePage">
 
