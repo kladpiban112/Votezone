f5403184d000f1f9480a0226d80e9a940bf2f3e4
kladpiban112 <kladpibanfong@gmail.com>
2022-02-28 10:49:37 +0700

    fong
    test

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "votezone_db";
+$dbname = "votezone_db"; 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
