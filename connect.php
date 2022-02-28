9e1ed72a67c7331d4140e159987461d758ba428d
kladpiban112 <kladpibanfong@gmail.com>
2022-02-28 10:57:54 +0700

    fong
    utf8

--- connect.php
+++ connect.php
@@ -6,6 +6,7 @@ $dbname = "votezone_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+$conn->query("SET NAMES 'utf8'");
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
--- reset.php
+++ reset.php
@@ -10,7 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
+$conn->query("SET NAMES 'utf8'");
 $conn->query("UPDATE campur SET zone='0'");
 $conn->query("UPDATE ctambon SET zone='0'");
 $conn->query("UPDATE zone SET flag='0'");
